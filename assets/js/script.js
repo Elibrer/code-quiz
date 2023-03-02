d05b783b374edc82ab05e49910246cee0d553b13
Elibrer <eli.brer@gmail.com>
2023-03-02 23:30:32 +1100

    Improved CSS, started adding some JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,21 @@
 
 
 
+var countdownTimer = document.getElementById("countdown");
+var startBtn = document.getElementById("start-button");
 
 
 
 
 //START BUTTON
 
+function begin() {
+    var hideIntro = document.getElementById("intro");
 
+
+
+
+}
 
 //TIMER
 
