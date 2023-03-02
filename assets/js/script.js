8c3481f5304bc5f758d53fb9732002c8493c5072
Elibrer <eli.brer@gmail.com>
2023-03-03 00:12:49 +1100

    Added working timer that starts with start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,50 @@
 
 
-
+var timerCount = 0;
+var time = 60;
 
 var countdownTimer = document.getElementById("countdown");
-var startBtn = document.getElementById("start-button");
-
+var startBtn = document.querySelector("#start-button");
+var quizCard = document.getElementById("quiz")
 
 
 
 //START BUTTON
 
 function begin() {
-    var hideIntro = document.getElementById("intro");
 
+    console.log(hideIntro);
+
+    var hideIntro = document.getElementById("intro");
+    hideIntro.setAttribute("class", "hide-card");
+
+    console.log(hideIntro);
+
+    quizCard.removeAttribute("class");
+
+    timerCount = setInterval(countdown, 1000);
+    console.log(countdown);
+
+    console.log("Started");
 
 
 
 }
 
 //TIMER
+function countdown() {
+time--
+countdownTimer.textContent = time;
+
+if (time <= 0) {
+    stopQuiz();
+}
+
+}
+
+
+
+
 
 //INIT GAME
 
@@ -26,10 +52,19 @@ function begin() {
 //TIME SUBTRACTION
 
 
+
+
 //SCORE TABLE
 
 
+//STOP THE QUIZ
+
+function stopQuiz() {
+    
+
+
+}
 
 
 
-
+startBtn.addEventListener("click", begin);
